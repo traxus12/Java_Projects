ba0671486b5192419d2a18b228fe7a9fc49f52b5
Traxus12 <andrew123daly@gmail.com>
2021-09-27 11:48:34 -0400

    progress, but incorrect insertions to puzzle

---
+++ .bash_history
@@ -0,0 +1,60 @@
+ls
+mkdir HelloWorld.java 
+ls -a
+mkdir HelloWorld
+cp HelloWorld.java HelloWorld
+ls
+rm HelloWorld
+rm HelloWorld.java
+clear
+cd HelloWorld/
+ls
+cd ..
+ls
+vi .gitignore
+rm .gitignore 
+clear
+git status
+rm .viminfo 
+clear
+ls
+ls -a
+cd HelloWorld/
+cd ..
+cd HelloWorld/
+ls -a
+clear
+cd ..
+clear
+ls
+cd DataTypes/
+ls
+ls -a
+clear
+cd ..
+git add .
+git commit -m "Added a HelloWorld file"
+git config --global user.name "Traxus12"
+git add .
+git commit -m "Added a HelloWorld file"
+git config --global user.email "andrew123daly@gmail.com"
+git add .
+git commit -m "Added a HelloWorld file"
+git push
+ls
+clear
+mkdir sudoko
+rm -rf sudoko/
+mkdir sudoku
+ls
+cd sudoku/
+ls
+pwd
+clear
+ls
+cd ..
+ls
+git add .
+git commit -m "first commit for sudoku solver"
+git push
+ls
--- sudoku/SudokuSolver.java
+++ sudoku/SudokuSolver.java
@@ -3,9 +3,69 @@ package sudoku;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.stream.Stream;
 
 public class SudokuSolver {
+    // returns false is k in col
+    public static boolean colCheck(int k, int[] col) {
+        for (int i = 0; i < col.length; i++) {
+            if (col[i] == k) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // returns false if k in row
+    public static boolean rowCheck(int k, int[] row) {
+        for (int i = 0; i < row.length; i++) {
+            if (row[i] == k) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean gridCheck(int k, int[][] grid) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (grid[i][j] == k) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // attempt to place k in the x y coordiante of puzzle, return true if
+    // permissible
+    public static boolean attemptK(int k, int[][] puzzle, int i, int j) {
+        if (rowCheck(k, puzzle[i])) {
+            if (colCheck(k, getColumn(puzzle, j))) {
+                int[][] grid = new int[3][3];
+                //There are 9 possible grids i j could be in
+                //int gridIndex = ((i  / 3)* 3 + (j / 3));
+                
+                for(int a = 0; a<3; a++){
+                    for(int b = 0; b<3; b++){
+                        grid[a][b] = puzzle[a+((i  / 3)* 3)][b+(j / 3)];
+                    }
+                }
+                if (gridCheck(k, grid)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static int[] getColumn(int[][] array, int index) {
+        int[] column = new int[array[0].length]; // Here I assume a rectangular 2D array!
+        for (int i = 0; i < column.length; i++) {
+            column[i] = array[i][index];
+        }
+        return column;
+    }
+
     public static void main(String[] args) throws IOException {
         // A sudoku grid is read in as a text file
         BufferedReader br = new BufferedReader(
@@ -25,8 +85,8 @@ public class SudokuSolver {
         }
         System.out.println("***************************************");
         everything = everything.replaceAll(",", "");
-        everything = everything.replaceAll("\\s+", "");
         System.out.println(everything);
+        everything = everything.replaceAll("\\s+", "");
         char[][] charPuzzle = new char[9][9];
         int index = 0;
         for (int i = 0; i < 9; i++) {
@@ -51,6 +111,29 @@ public class SudokuSolver {
             for (int j = 0; j < 9; j++) {
                 System.out.print(intPuzzle[i][j]);
             }
+            System.out.println("");
         }
+        System.out.println("");
+
+        int gridIndex = ((8  / 3)* 3 + (8 / 3));
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (intPuzzle[i][j] == 0) {
+                    for (int k = 0; k < 9; k++) {
+                        boolean placeK =  attemptK(k, intPuzzle, i, j);
+                        if (placeK) {
+                            intPuzzle[i][j] = k;
+                        }
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(intPuzzle[i][j]+" ");
+            }
+            System.out.println("");
+        }
+
     }
 }
